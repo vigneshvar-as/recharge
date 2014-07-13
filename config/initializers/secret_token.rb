6cb3b781e6a2b20f3dc84b18d235f4a95b827f20
viggates <vigneshvar.a.s@gmail.com>
2014-07-13 15:40:43 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Recharge::Application.config.secret_key_base = '189a16344f157932ee58b6311d583276d8082e3c6866bd9b370c9a820d97fb8aa38c71a5de542ddea72250abf9820f797d623c56fdbc547fb48bf5530f2e622d'
